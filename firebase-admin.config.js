5f22b16dcd363553a94383de9e908a7540356d6b
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-05-10 15:47:05 +0800

    changed private key access

--- firebase-admin.config.js
+++ firebase-admin.config.js
@@ -2,11 +2,13 @@ import * as firebase from "firebase-admin/app";
 import "firebase-admin/auth";
 import "firebase-admin/firestore";
 
+const { privateKey } = JSON.parse(process.env.PRIVATE_KEY);
+
 if (!firebase.getApps().length) {
     firebase.initializeApp({
         credential: firebase.cert({
             projectId: process.env.PROJECT_ID,
-            privateKey: process.env.PRIVATE_KEY,
+            privateKey: privateKey,
             clientEmail: process.env.CLIENT_EMAIL,
         }),
     });
